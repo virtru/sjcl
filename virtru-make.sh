d561f1e850dbc8aa52357e96acc1a0114842e284
Zack Nelson <znelson@virtru.com>
2016-08-23 21:29:10 -0400

    adds compiled version that includes the feature used by virtru;

---
+++ virtru-make.sh
@@ -0,0 +1,5 @@
+# Configure the components that are in use
+./configure --without-ccm --without-ocb2 --without-codecBase32 --without-pbkdf2 --with-cbc --with-codecBytes --without-codecBase64 --with-codecArrayBuffer
+
+# Make the output file
+make
